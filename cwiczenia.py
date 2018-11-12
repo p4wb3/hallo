08a955d104e00d7accffca68ad342786edcbe366
Your Name <pawel83.pb@gmail.com>
2018-11-12 18:46:21 +0100

    new branch

--- cwiczenia.py
+++ cwiczenia.py
@@ -63,7 +63,7 @@ firstList = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 secondList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 
 import random
-input('Podaj liczbe : ')
+
 firstList.extend(secondList)
 newList = list(set(firstList))
 newList.sort()
@@ -77,3 +77,5 @@ randomList.sort()
 newList.extend(randomList)
 randomList=list(set(newList))
 print(randomList)
+
+
